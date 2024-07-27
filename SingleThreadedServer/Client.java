31863131b77621816eb9458ab92c2af9627a0375
sandesh300 <bhujbalsandesh52@gmail.com>
2024-07-27 23:40:20 +0530

    fix Client and Server Code, add exception

--- SingleThreadedServer/Client.java
+++ SingleThreadedServer/Client.java
@@ -6,23 +6,23 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-
+import java.net.UnknownHostException;
 
 public class Client {
 
-    public void run() throws IOException {
-       int port = 8010;
-       InetAddress address = InetAddress.getByName("localhost");
-       Socket socket = new Socket(address, port);
-       PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
-       BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-       toSocket.println("Hello from the Client "+socket.getLocalSocketAddress());
-       String line = fromSocket.readLine();
-       toSocket.close();
-       fromSocket.close();
+    public void run() throws IOException, UnknownHostException {
+        int port = 8010;
+        InetAddress address = InetAddress.getByName("localhost");
+        Socket socket = new Socket(address, port);
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello from the Client! " + socket.getLocalSocketAddress());
+        String line = fromSocket.readLine();
+        toSocket.close();
+        fromSocket.close();
         socket.close();
     }
-    
+
     public static void main(String[] args) {
         Client client = new Client();
         try {
--- SingleThreadedServer/Server.java
+++ SingleThreadedServer/Server.java
@@ -6,21 +6,22 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Server {
 
-    public void run() throws IOException{
+    public void run() throws IOException, UnknownHostException{
         int port = 8010;
         ServerSocket socket = new ServerSocket(port);
         socket.setSoTimeout(10000);
         while (true) {
             try {
-                System.out.println("Server is listening on port"+port);
+                System.out.println("Server is listening on port : "+port);
                 Socket acceptedConnection = socket.accept();
-                System.out.println("Connection accepted from client" + acceptedConnection.getRemoteSocketAddress());
+                System.out.println("Connection accepted from client " + acceptedConnection.getRemoteSocketAddress());
                 PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
                 BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
-                toClient.println("Hello from the server");
+                toClient.println("Hello from the server! ");
                 toClient.close();
                 fromClient.close();
                 acceptedConnection.close();
