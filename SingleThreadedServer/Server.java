17bfc394961488bd62547808f8003ddd6ba0fd87
sandesh300 <bhujbalsandesh52@gmail.com>
2024-07-27 22:42:51 +0530

    create single thread server

---
+++ SingleThreadedServer/Server.java
@@ -0,0 +1,15 @@
+package SingleThreadedServer;
+
+import java.net.ServerSocket;
+
+public class Server {
+
+    public void run() {
+        int port = 8010;
+        ServerSocket socket = new ServerSocket(port);
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
